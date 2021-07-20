d7a32fe92d1baeef33edc99f3354920d4c0e32ef
coolcatcoder <51190960+coolcatcoder@users.noreply.github.com>
2021-07-20 10:02:15 +0800

    Create main.js

---
+++ magazine/main.js
@@ -0,0 +1,29 @@
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+function rainbow() {
+  document.getElementById('main_title').style.color = "red";
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "orange";
+  }, 200);
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "yellow";
+  }, 400);
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "green";
+  }, 600);
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "blue";
+  }, 800);
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "purple";
+  }, 1000);
+  setTimeout(function(){
+    document.getElementById('main_title').style.color = "Chartreuse";
+  }, 1200);
+}
