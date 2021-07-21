a9ca8a3fe65356996b93afbbb9a0a995c5fbf255
coolcatcoder <coolcatcal5@gmail.com>
2021-07-21 10:06:34 +0800

    Update main.js

--- magazine/article/unity_vs_unreal/main.js
+++ magazine/article/unity_vs_unreal/main.js
@@ -1,11 +1,11 @@
 function annoy() {
-  getElementById('awful').innerHTML = "<p id=\"annoying\">To view this article please click \"allow\" to confirm you are not a robot.</p>";
+  document.getElementById('awful').innerHTML = "<p id=\"annoying\">To view this article please click \"allow\" to confirm you are not a robot.</p>";
   Notification.requestPermission().then(funtion (permission) {
     if (permission === "granted") {
       var notification = new Notification("Thank you for subscribing to our newsletter!");
-      getElementById('awful').innerHTML = "<p id=\"annoying\">Thank you for confirming that you are not a robot</p>";
+      document.getElementById('awful').innerHTML = "<p id=\"annoying\">Thank you for confirming that you are not a robot</p>";
     } else {
-      getElementById('awful').innerHTML = "<p id=\"annoying\">You did not press allow, as such we cannot confirm that you are not a robot.</p>";
+      document.getElementById('awful').innerHTML = "<p id=\"annoying\">You did not press allow, as such we cannot confirm that you are not a robot.</p>";
     }
   });
 }
