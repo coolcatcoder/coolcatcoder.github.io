c5b8412938eaf0d93337ff6f80a19fa3db7522b3
coolcatcoder <coolcatcal5@gmail.com>
2021-07-21 09:50:50 +0800

    Update main.js

--- magazine/article/unity_vs_unreal/main.js
+++ magazine/article/unity_vs_unreal/main.js
@@ -1,3 +1,3 @@
 function annoy() {
-  getElementById('awful').innerHTML = "<p id="annoying">To view this article please click \"allow\" to confirm you are not a robot.</p>";
+  getElementById('awful').innerHTML = "<p id=\"annoying\">To view this article please click \"allow\" to confirm you are not a robot.</p>";
 }
