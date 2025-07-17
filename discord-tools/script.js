51f1d7aeb13007e38b3a8c507f7d76068ce3152f
Katt <51190960+coolcatcoder@users.noreply.github.com>
2025-07-17 18:23:56 +0800

    make it regional converter work

--- discord-tools/script.js
+++ discord-tools/script.js
@@ -2,7 +2,15 @@ function regional_convert() {
     let input = document.getElementById("regional_input").value;
     let output = ""
     for (let i = 0; i < input.length; i++) {
-        output += ":regional_indicator_" + input.charAt(i) + ":";
+        let input_char = input.charAt(i);
+        if (/[a-zA-Z]/.test(input_char)) {
+            output += ":regional_indicator_" + input_char.toLowerCase() + ":";
+        } else if (/\d/.test(input_char)) {
+            output += output += ":number_" + input_char.toLowerCase() + ":";;
+        } else {
+            output += input_char;
+        }
+        
     }
     document.getElementById("regional_input").value = output;
 }
