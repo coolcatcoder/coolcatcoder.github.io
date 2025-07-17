f7a87b17192ea0c4530235898dd5e44b41f37e9f
Katt <51190960+coolcatcoder@users.noreply.github.com>
2025-07-17 19:28:48 +0800

    add no flags option

--- discord-tools/script.js
+++ discord-tools/script.js
@@ -1,10 +1,16 @@
 function regional_convert() {
     let input = document.getElementById("regional_input").value;
+    let no_flags = document.getElementById("no_flags").value;
+    console.log(no_flags);
+
     let output = ""
     for (let i = 0; i < input.length; i++) {
         let input_char = input.charAt(i);
         if (/[a-zA-Z]/.test(input_char)) {
             output += ":regional_indicator_" + input_char.toLowerCase() + ":";
+            if (no_flags == "on") {
+                output += '​';
+            }
             console.log(input_char);
         } else if (/\d/.test(input_char)) {
             output += output += ":number_" + input_char.toLowerCase() + ":";;
