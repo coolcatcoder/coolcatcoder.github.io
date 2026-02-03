cd63674398287309b917a7561077dcddaa51b687
Katt <51190960+coolcatcoder@users.noreply.github.com>
2026-02-03 20:39:00 +0800

    Switch discord tools to rust.

--- discord-tools/script.js
+++ discord-tools/script.js
@@ -1,25 +1,13 @@
+import init, * as wasm from "./pkg/discord_tools.js";
+
+await init();
+
 function regional_convert() {
     let input = document.getElementById("regional_input").value;
     let no_flags = document.getElementById("no_flags").checked;
-    console.log(no_flags);
 
-    let output = ""
-    for (let i = 0; i < input.length; i++) {
-        let input_char = input.charAt(i);
-        if (/[a-zA-Z]/.test(input_char)) {
-            output += ":regional_indicator_" + input_char.toLowerCase() + ":";
-            if (no_flags) {
-                output += '​';
-            }
-            console.log(input_char);
-        } else if (/\d/.test(input_char)) {
-            output += output += ":number_" + input_char.toLowerCase() + ":";;
-        } else if (input_char == ':') {
-            output += "\:";
-        } else {
-            output += input_char;
-        }
-        
-    }
+    let output = wasm.convert_string_to_regionals(input, no_flags);
+
     document.getElementById("regional_input").value = output;
 }
+window.regional_convert = regional_convert;
